1cd7aa35ef4623e7162c9321e78082acc71d65d3
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-06-06 16:14:50 -0400

    commit tree ce2565710f6f75d8e1e3e23c4659d3edc9c8ad57

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,13 @@
 class Student < ApplicationRecord
 
+  scope :by_name, ->(name) { where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%") }
+
   def to_s
     "#{first_name} #{last_name}"
   end
+
+  # def self.by_name name
+  #   Student.all.where("first_name == '#{name}'").or(Student.all.where("last_name == '#{name}'"))
+  # end
 
 end
